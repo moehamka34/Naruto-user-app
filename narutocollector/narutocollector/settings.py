fa826cf77da35f580cb0d8299fd336060c39fd61
Mohamed Hamka <mohamedhamka@Mohameds-MBP-2.attlocal.net>
2022-07-13 16:52:51 -0400

    Config

--- narutocollector/narutocollector/settings.py
+++ narutocollector/narutocollector/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,7 +126,7 @@ LOGOUT_REDIRECT_URL = '/accounts/login/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-WSGI_APPLICATION = 'narutocollector.wsgi.application'
+
 # catcollector is the project name
-import django_heroku
+
 django_heroku.settings(locals())
